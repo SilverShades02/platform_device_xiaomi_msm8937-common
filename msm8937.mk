ps217/android_device_xiaomi_msm8937-common

msm8937-common: set override maintainers

msm8937.mk
@@ -405,6 +405,10 @@ PRODUCT_PACKAGES += \
  PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# Maintainers
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.fred.maintainer=StrangeNoob|Rainforce279
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Inherit common proprietary files

SilverShades02/platform_device_xiaomi_msm8937-common

msm8937.mk
@@ -405,6 +405,10 @@ PRODUCT_PACKAGES += \
  PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# Maintainers
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.fred.maintainer=PS217|StrangeNoob
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Inherit common proprietary files